096e57b7b2ef6cb11e473746fc4c4e2cde7a03b7
Roman Inflianskas <infroma@gmail.com>
2018-11-11 11:16:22 +0300

    Support tomlkit>=0.4.6,<0.6.9

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ package_data = \
 {'': ['*']}
 
 install_requires = \
-['tomlkit>=0.4.6,<0.5.0']
+['tomlkit>=0.4.6,<0.6.0']
 
 setup_kwargs = {
     'name': 'poetry-version',
-    'version': '0.1.1',
+    'version': '0.1.2',
     'description': 'Python library for extracting version from poetry pyproject.toml file',
     'long_description': '# poetry-version\nPython library for extracting version from poetry pyproject.toml file\n\n## Installation\nTo install `poetry-version` from [PyPI](https://pypi.org/project/poetry-version/) run:\n```shell\n$ pip install poetry-version\n```\n\n## Usage\nPut these lines somewhere in the main module:\n```python\nimport poetry_version\n\n__version__ = poetry_version.extract(source_file=__file__)\n```\n',
     'author': 'Roman Inflianskas',
